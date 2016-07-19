7db4f63bebe878113285755c3d79b2af098d56d2
Krzysztof Kowalczuk <kowal012@gmail.com>
2016-07-19 18:54:16 +0200

    Changed class Name and lowercase for Uppercase letters.

--- StudentManagerWebApp/Login.aspx.cs
+++ StudentManagerWebApp/Login.aspx.cs
@@ -9,27 +9,27 @@ namespace StudentManagerWebApp
 {
     public partial class Login : System.Web.UI.Page
     {
-        List<user> Users = new List<user>();
+        List<User> Users = new List<User>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Users.Add(new user("s1", "123", "student", 1));
-            Users.Add(new user("s2", "123", "student", 2));
-            Users.Add(new user("s3", "123", "student", 3));
-            Users.Add(new user("s4", "123", "student", 4));
-            Users.Add(new user("s5", "123", "student", 5));
-            Users.Add(new user("t1", "123", "teacher", 6));
+            Users.Add(new User("s1", "123", "student", 1));
+            Users.Add(new User("s2", "123", "student", 2));
+            Users.Add(new User("s3", "123", "student", 3));
+            Users.Add(new User("s4", "123", "student", 4));
+            Users.Add(new User("s5", "123", "student", 5));
+            Users.Add(new User("t1", "123", "teacher", 6));
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {           
-            foreach (user u in Users)
+            foreach (User u in Users)
             {
-                if (loginBox.Text == u.username && passwordBox.Text == u.password)
+                if (loginBox.Text == u.Username && passwordBox.Text == u.Password)
                 {
                     Session["Login"] = loginBox.Text;
-                    Session["Role"] = u.role;
-                    Session["ID"] = u.id;                  
+                    Session["Role"] = u.Role;
+                    Session["ID"] = u.Id;                  
                     break;
                 }                                
             }
@@ -50,18 +50,18 @@ namespace StudentManagerWebApp
         }
     }
 
-    class user
+    class User
     {
-        public string username;
-        public string password;        
-        public string role;
-        public int id;
-        public user(string Username, string Password, string Role, int Id)
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int Id { get; set; }
+        public User(string username, string password, string role, int id)
         {            
-            username = Username;
-            password = Password;
-            role = Role;
-            id = Id;
+            Username = username;
+            Password = password;
+            Role = role;
+            Id = id;
         }
     }
 }
